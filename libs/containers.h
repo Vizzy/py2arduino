5584c4bb27fbba39597917bb5f467fbc6347da36
Anton Osten <anton@ostensible.me>
2013-07-06 06:10:44 +0400

    more container stuff

--- libs/containers.h
+++ libs/containers.h
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
 template <class T>
 class List;
@@ -97,6 +97,35 @@ public:
 		return false;
 	}
 
+	int index(T elem)
+	{
+		if (length > 0)
+		{
+			Node<T> *cur_elem = &root_elem;
+			int ind = 0;
+
+			while (cur_elem != NULL)
+			{
+				if (cur_elem->data == elem)
+				{
+					return ind;
+				}
+				else
+				{
+					cur_elem = cur_elem->next;
+					ind++;
+				}
+			}
+
+			// if the loop runs out
+			return -1;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
 	void append(T elem)
 	{
 		Node<T> new_elem;
@@ -115,8 +144,66 @@ public:
 		length++;
 	}
 
+	void remove(const T elem)
+	{
+		if (length > 0)
+		{
+			int ind = index(elem);
+
+			if (ind == -1)
+			{
+				return;
+			}
+
+			if (ind == 0)
+			{
+				Node<T> *root = &root_elem;
+				root = root->next;
+			}
+			else if (ind > 0)
+			{
+				int count = 1;
+				Node<T> *cur_elem = &root_elem;
+
+				while (count < ind)
+				{
+					cur_elem = cur_elem->next;
+				}
+
+				cur_elem->next = cur_elem->next->next;
+			}
+		}
+
+		length--;
+	}
+
+	int operator[] (const int ind)
+	{
+		Node<T> *theElem = get_elem_at_index(ind);
+		return theElem->data;
+	}
+
+	void print()
+	{
+		std::cout << "[";
+
+		for (int i = 0; i < length; ++i)
+		{
+			Node<T> *elem = get_elem_at_index(i);
+			std::cout << elem->data; 
+
+			if (i + 1 < length)
+			{
+				std::cout << ", ";
+			}
+		}
+
+		std::cout << "]\n";
+	}
+
 	int length;
 	Node<T> root_elem;
+	typedef T type;
 
 private:
 	Node<T> *get_current_elem()
@@ -128,15 +215,38 @@ private:
 		}
 		return cur_elem;
 	}
+
+	Node<T> *get_elem_at_index(const int ind)
+	{
+		if (length > 0 && ind < length)
+		{
+			Node<T> *cur_elem = &root_elem;
+			int count = 0;
+
+			while (count < ind)
+			{
+				cur_elem = cur_elem->next;
+				count++;
+			}
+
+			return cur_elem;
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+
+protected:
+	T *as_array()
+	{
+		T *arr = malloc(length * sizeof(T));
+
+		for (int i = 0; i < length; i++)
+		{
+			arr[i] = this[i];
+		}
+
+		return arr;
+	}
 };
-
-int main(int argc, char const *argv[])
-{
-	List<int> myList;
-	myList.append(1);
-	myList.append(5);
-	cout << noboolalpha << myList.contains(5) << "\n";
-	cout << noboolalpha << myList.contains(3) << "\n";
-
-	return 0;
-}
